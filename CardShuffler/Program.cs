ef74987ae5cc691e87ca97a4a9e8d2c42ef25d0f
C D <u305543@outlook.com>
2017-04-16 12:06:10 -0400

    Created Class Deck to put all the deck mehods together.

--- CardShuffler/Program.cs
+++ CardShuffler/Program.cs
@@ -10,58 +10,21 @@ namespace CardShuffler
 {
     class Program
     {
+    
         static void Main(string[] args)
         {
-            ArrayList deck = createDeck();
 
-            writeToConsole(deck);
+            Deck deck = new Deck();
 
-            shuffleDeck(deck);
+            ArrayList cards = deck.createDeck();
 
-            writeToConsole(deck);
+            deck.writeToConsole(cards);
 
-            shuffleDeck(deck);
+            deck.shuffleDeck(cards);
 
-            writeToConsole(deck);
+            deck.writeToConsole(cards);
+        
         }
-        static public void shuffleDeck(ArrayList arrlist)
-        {
-            Random r = new Random();
-
-            for (int i = 0; i < arrlist.Count; i++)
-            {
-                object tmp = arrlist[i];
-                int idx = r.Next(arrlist.Count - i) + i;
-                arrlist[i] = arrlist[idx];
-                arrlist[idx] = tmp;
-            }
-        }
-
-        static public ArrayList createDeck()
-        {
-            string[] suits = { "H", "D", "S", "C" };
-            string[] values = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
-            ArrayList deck = new ArrayList();
-
-            foreach (string suit in suits)
-            {
-                foreach (string value in values)
-                {
-                    deck.Add(value + suit);
-                }
-            }
-
-            return deck;
-        }
-
-        static private void writeToConsole(ArrayList deck)
-        {
-            foreach (string card in deck)
-            {
-                Console.Write(card + " ");
-            }
-            Console.Write("\n");
-            Console.WriteLine("Total cars in deck: " + deck.Count);
-        }
+        
     }
 }
